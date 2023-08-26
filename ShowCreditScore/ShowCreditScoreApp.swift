69dc9470ba7ad438c6066ce983f213b4c0f70944
ManiYechuri <maniyechuri09@gmail.com>
2023-08-26 06:25:35 +0200

    Initial Commit

---
+++ ShowCreditScore/ShowCreditScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowCreditScoreApp.swift
+//  ShowCreditScore
+//
+//  Created by Mani Yechuri on 2023/08/26.
+//
+
+import SwiftUI
+
+@main
+struct ShowCreditScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
